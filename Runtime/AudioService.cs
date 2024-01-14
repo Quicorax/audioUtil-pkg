81e19016ba35b013a23f85d1b2674bfe63cdecfb
Quicorax <qcolobrans@gmail.com>
2024-01-14 12:20:02 +0100

    Async Service Initialization

--- Runtime/AudioService.cs
+++ Runtime/AudioService.cs
@@ -6,35 +6,61 @@ namespace Services.Runtime.AudioService
     public class AudioService : IAudioService
     {
         private readonly AudioNest _audioNest;
+        private bool _isReady;
         
         public AudioService()
         {
-            _audioNest = new GameObject().AddComponent<AudioNest>().Initialize(FetchDependencies());
+            _audioNest = new GameObject().AddComponent<AudioNest>();
+    
+            var dependencies = Resources.LoadAsync("Audio/AudioDependencies");
+            dependencies.completed += _ => SetDependencies(dependencies);
         }
-        
-        public void PlaySFX(string sfxKey) => _audioNest.PlaySFX(sfxKey);
-        public void PlayMusic(string musicKey) => _audioNest.PlayMusic(musicKey);
-        public void StopMusic(string musicKey, float fadeTime = 0, Action onComplete = null) => _audioNest.StopMusic(musicKey, fadeTime, onComplete);
-        public void TransitionMusic(string fromMusicKey, string toMusicKey, float fadeTime = 0) => _audioNest.StopMusic(fromMusicKey, fadeTime, ()=> PlayMusic(toMusicKey));
-        public void StopAllMusics(float fadeTime) => _audioNest.StopAllMusics(fadeTime);
-        public void AddMasterVolume(float additiveValue) => _audioNest.Volume.AddMasterVolume(additiveValue);
-        public void AddMusicVolume(float additiveValue) => _audioNest.Volume.AddMusicVolume(additiveValue);
-        public void AddSFXVolume(float additiveValue) => _audioNest.Volume.AddSFXVolume(additiveValue);
-        public bool MuteMaster() => _audioNest.Volume.MuteMaster();
-        public bool MuteMusic() => _audioNest.Volume.MuteMusic();
-        public bool MuteSFX() => _audioNest.Volume.MuteSFX();
-        public void ClearAudio() => _audioNest.ClearAudio();
-
-        private AudioDependencies FetchDependencies()
+    
+        public void PlaySFX(string sfxKey) => IsReady(()=> _audioNest.PlaySFX(sfxKey));
+        public void PlayMusic(string musicKey) => IsReady(()=> _audioNest.PlayMusic(musicKey));
+        public void StopMusic(string musicKey, float fadeTime = 0, Action onComplete = null) => IsReady(()=> _audioNest.StopMusic(musicKey, fadeTime, onComplete));
+        public void TransitionMusic(string fromMusicKey, string toMusicKey, float fadeTime = 0) => IsReady(()=> _audioNest.StopMusic(fromMusicKey, fadeTime, () => PlayMusic(toMusicKey)));
+        public void StopAllMusics(float fadeTime) => IsReady(()=> _audioNest.StopAllMusics(fadeTime));
+        public void AddMasterVolume(float additiveValue) => IsReady(()=> _audioNest.Volume.AddMasterVolume(additiveValue));
+        public void AddMusicVolume(float additiveValue) => IsReady(()=> _audioNest.Volume.AddMusicVolume(additiveValue));
+        public void AddSFXVolume(float additiveValue) => IsReady(()=> _audioNest.Volume.AddSFXVolume(additiveValue));
+        public void ClearAudio() => IsReady(()=> _audioNest.ClearAudio());
+        public bool MuteMaster() => IsReady(() => _audioNest.Volume.MuteMaster());
+        public bool MuteMusic() => IsReady(() => _audioNest.Volume.MuteMusic());
+        public bool MuteSFX() => IsReady(() => _audioNest.Volume.MuteSFX());
+    
+        private void SetDependencies(ResourceRequest asset)
         {
-            var dependencies = Resources.Load<AudioDependencies>("AudioDependencies");
-
-            if (dependencies == null)
+            if (asset == null)
             {
                 Debug.LogError("No Audio dependencies defined in the Resources folder!");
             }
-            
-            return dependencies;
+    
+            _audioNest.Initialize(asset?.asset as AudioDependencies);
+            _isReady = true;
+        }
+    
+        private void IsReady(Action onReady)
+        {
+            if (_isReady)
+            {
+                onReady.Invoke();
+                return;
+            }
+    
+            Debug.LogWarning("AudioService is not ready. Skipped call");
+        }
+        
+        private T IsReady<T>(Func<T> onReady)
+        {
+            if (_isReady)
+            {
+               return onReady.Invoke();
+            }
+    
+            Debug.LogWarning("AudioService is not ready. Skipped call");
+    
+            return default;
         }
     }
 }
